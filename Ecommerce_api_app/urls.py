05a7c0aa82dfb0444afc3dfbec0d2509adbba9a9
manucho254 <manuchoadero@gmail.com>
2021-10-31 18:34:11 +0300

    search

--- Ecommerce_api_app/urls.py
+++ Ecommerce_api_app/urls.py
@@ -3,7 +3,7 @@ from .views import ProductList,ProductDetail, CategoryDetail,search
 
 urlpatterns = [
     path('product/',  ProductList.as_view(), name="products"),
-    path('products/search', search),
+    path('products/search/', search),
     path('products/<slug:product_category_slug>/<slug:product_slug>/',  
           ProductDetail.as_view(), name="product-detail"),
     path('products/<slug:product_category_slug>/' , CategoryDetail.as_view()),
