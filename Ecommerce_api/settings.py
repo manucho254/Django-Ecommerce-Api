ba3e02f3fd42930d45786135315fb4eabe1bbd18
manucho254 <manuchoadero@gmail.com>
2021-11-03 14:23:30 +0300

    login

--- Ecommerce_api/settings.py
+++ Ecommerce_api/settings.py
@@ -35,7 +35,6 @@ INSTALLED_APPS = [
     'corsheaders',
     'djoser',
     'accounts',
-    'vuefrontend',
 ]
 
 CORS_ALLOWED_ORIGINS = [
--- Ecommerce_api/urls.py
+++ Ecommerce_api/urls.py
@@ -6,4 +6,6 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include("Ecommerce_api_app.urls")),
+    path('api/v1/', include('djoser.urls')),
+    path('api/v1/', include('djoser.urls.authtoken')),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
